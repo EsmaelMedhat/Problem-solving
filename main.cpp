54dedcb2a158d78f8df3e886ca43158f05e0568b
Esmael Medhat <155559209+EsmaelMedhat@users.noreply.github.com>
2024-01-19 19:45:05 +0200

    is num1 divisble by num2 and num3

---
+++ main.cpp
@@ -0,0 +1,9 @@
+bool isDivisible(int num1, int num2, int num3) {
+bool is_Divisible;
+if(num1%num2==0&&num1%num3==0)
+is_Divisible=true;
+else{
+  is_Divisible=false;
+}
+    return is_Divisible;
+}
